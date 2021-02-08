e9c74e8159bcffe5346274025aea289881902d10
djarosz-hub <jaroszdamian9@gmail.com>
2021-02-08 01:53:28 +0100

    added all pages and navigation

--- WpfApp1/OSHome.xaml.cs
+++ WpfApp1/OSHome.xaml.cs
@@ -20,9 +20,51 @@ namespace WpfApp1
     /// </summary>
     public partial class OSHome : Page
     {
+        readonly OrdersPage ordersPage;
+        readonly CategoriesPage categoriesPage;
+        readonly ProducersPage producersPage;
+        readonly ProductsPage productsPage;
+        readonly EmployeesPage employeesPage;
+        readonly ClientsPage clientsPage;
         public OSHome()
         {
             InitializeComponent();
+            ordersPage = new OrdersPage();
+            categoriesPage = new CategoriesPage();
+            producersPage = new ProducersPage();
+            productsPage = new ProductsPage();
+            employeesPage = new EmployeesPage();
+            clientsPage = new ClientsPage();
+        }
+
+        private void OrderButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.NavigationService.Navigate(ordersPage);
+        }
+
+        private void CategoriesButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.NavigationService.Navigate(categoriesPage);
+        }
+
+        private void ProducersButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.NavigationService.Navigate(producersPage);
+        }
+
+        private void ProductsButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.NavigationService.Navigate(productsPage);
+        }
+
+        private void EmployeesButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.NavigationService.Navigate(employeesPage);
+        }
+
+        private void ClientsButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.NavigationService.Navigate(clientsPage);
         }
     }
 }
